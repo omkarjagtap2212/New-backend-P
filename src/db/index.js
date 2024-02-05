861a42e9d8296bbde6099b69403465e7608c5a73
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-02-05 21:47:26 +0530

    connec- mongoatlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+
+
+
+const connectDB = async () => {
+    try {
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // console.log(connectInstance)
+        console.log(`\n MongoDB connected !! DB HOST :${connectInstance.connection.host}`)
+
+    } catch (error) {
+        console.log("db connection error", error)
+        process.exit(1)
+
+    }
+} 
+
+export default connectDB
