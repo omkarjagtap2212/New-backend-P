7496fbc5bfc322deaa806b99384711150068b2ac
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-02-07 02:46:59 +0530

    usermodels and videomodels inside methods and pluggin inside mongomodel

---
+++ src/models/video.model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; 
+
+const videoSchema = new mongoose.Schema({
+
+
+    videoFile: {
+        type: String,
+        required: [true, "file is required"],
+
+    },
+
+    thumbnail: {
+        type: String,
+        required: true,
+
+    },
+
+    title: {
+        type: String,
+        required: true,
+
+    },
+
+    description: {
+        type: String,
+        required: true,
+
+    },
+    duration: {
+        type: String,  //get form clodinaryurl
+        required: true,
+
+    },
+    views: {
+        type: Number,  //get form clodinaryurl
+        default: 0
+
+    },
+    isPublished: {
+        type: Boolean,  //get form Flodinaryurl
+        default: true
+
+    },
+
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+
+
+
+
+}, { timestamps: true })
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
