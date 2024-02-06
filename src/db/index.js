cf42474ab74fadda6d6f05078274732fc982c017
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-02-07 01:12:07 +0530

    added utlities file inside utility folder

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        // console.log(connectInstance)
+        // console.log(connectInstance)im
         console.log(`\n MongoDB connected !! DB HOST :${connectInstance.connection.host}`)
 
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 // require("dotenv").config({path:"./env"})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:"./env"
 })
 
 
-
+const PORT=process.env.PORT || 7000
 
 
 
 connectDB()
+.then(()=>{
+    app.listen(PORT,()=>console.log("your server has been started at :",PORT))
+    
+
+})
+.catch((err)=>{
+    console.log("mongodb connection has been failed.!!",err)
+
+})
 
 
 
